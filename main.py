9c19e3bd540934c2222fc3d476bae56d4606e7b1
Richielly <richielly.positivo@gmail.com>
2023-07-13 11:02:18 -0300

    V_0.0.3

--- main.py
+++ main.py
@@ -13,9 +13,22 @@ pag.click(patrimonio)
 nome_arquivo = r'C:\Users\Equiplano\PycharmProjects\ConversaoPorDigitacao\dist\Bem.txt'
 
 bem = pag.locateCenterOnScreen(r'images\bem.png', confidence=0.9)
-
+exist = pag.locateCenterOnScreen(r'images\exist.png', confidence=0.9)
 # if (bem is None):
 #     continue
+
+def seq_tab(parm):
+    for step in range(parm):
+        pag.press('tab')
+def choice(parm):
+    for step in range(parm):
+        pag.press('down')
+def tab(inf):
+    if not inf:
+        pag.press('tab')
+    else:
+        pag.press('tab')
+        pag.write(inf)
 
 print(rf.ler_arquivo_texto(nome_arquivo))
 pag.click(bem)
@@ -28,49 +41,40 @@ for linha in rf.ler_arquivo_texto(nome_arquivo):
     plaqueta = linha[2]
     simam = linha[3]
     data_inclusao_simam = linha[4]
-    detalhamento = ''
-    propriedade = ''
+    detalhamento = 'Fonógrafo'
+    propriedade = 'próprio'
     data_aquisicao = linha[5]
     descricao = linha[6]
 
     pag.write(codigo)
-    pag.press('tab')
-    pag.write(nome)
-    pag.press('tab')
-    pag.write(plaqueta)
-    pag.press('tab')
-    pag.write(simam)
-    pag.press('tab')
-    pag.write(data_inclusao_simam)
-    pag.press('tab')
-    pag.press('down')
-    pag.press('tab')
-    pag.press('tab')
-    pag.press('tab')
-    pag.press('tab')
-    pag.press('down')
-    pag.press('tab')
-    pag.write(data_aquisicao)
-    pag.press('tab')
-    pag.press('down')
-    pag.press('tab')
-    pag.press('down')
-    pag.press('tab')
-    pag.press('down')
-    pag.press('tab')
-    pag.write(descricao)
+    if not (exist is None):
+        tab(nome)
+        tab(plaqueta)
+        tab(simam)
+        tab(data_inclusao_simam)
+        tab(detalhamento)
+        seq_tab(3)
+        tab(propriedade)
+        tab(data_aquisicao)
+        seq_tab(1)
+        choice(1)
+        seq_tab(1)
+        choice(1)
+        seq_tab(1)
+        choice(1)
+        tab(descricao)
 
-    salvar = pag.locateCenterOnScreen(r'images\salvar.png', confidence=0.9)
-    pag.click(salvar)
+        salvar = pag.locateCenterOnScreen(r'images\salvar.png', confidence=0.9)
+        pag.click(salvar)
 
-    novo = pag.locateCenterOnScreen(r'images\novo.png', confidence=0.9)
-    pag.click(novo)
-
-
+        novo = pag.locateCenterOnScreen(r'images\novo.png', confidence=0.9)
+        pag.click(novo)
+    else:
+        novo = pag.locateCenterOnScreen(r'images\novo.png', confidence=0.9)
+        pag.click(novo)
 # pag.hotkey('f2')
 
-# pag.alert('Programa finalizado!')
-
+pag.alert('Programa finalizado!')
 
 #  pyinstaller --name digitacao --onefile --icon=img.ico --noconsole main.py
 # flet pack --name redirect_port --icon=img.ico main.py
